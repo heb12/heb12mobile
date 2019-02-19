660dfa91f41d2d2c52a635c9bbecb12a733383f8
Petabyte Studios <32369330+pufflegamerz@users.noreply.github.com>
2019-02-18 20:58:55 -0500

    Add files via upload

---
+++ docs/javascript.js
@@ -0,0 +1,11 @@
+var x, y;
+window.onload = function() {
+	setInterval(function() {
+		document.getElementById('main').style.backgroundPosition = x + "px " + y + "px";
+	},1);
+}
+
+function getMouse(event) {
+	x = event.clientX/60 - 10;
+	y = event.clientY/20 - 100;
+}
