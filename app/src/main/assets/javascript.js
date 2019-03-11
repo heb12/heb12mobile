3323a43a9f7a58239fe47647247938ca2e8ab735
Pufflegamerz <myneweb14>
2019-03-11 17:36:56 -0500

    Not use JS template strings

--- app/src/main/assets/javascript.js
+++ app/src/main/assets/javascript.js
@@ -266,55 +266,50 @@ function notify(text) {
 	var popup = document.getElementById('popupContent');
 	
 	if (text == "firsttime") {
-		popup.innerHTML = `
-		<h2>Welcome to Heb12!</h2>
-		<p>Heb12 Mobile is a free open-sourced app designed to make reading the Bible easy and hassle-free. If you would like to contribute or give feedback, please visit the&nbsp;<a href="https://github.com/heb12/heb12-mobile" target="_blank" rel="noopener">Github repository</a>.</p>
-		<p><strong>Pro Tip: Tap on a bible verse for a list of extra actions.</strong></p>`;
+		popup.innerHTML = '<h2>Welcome to Heb12!</h2>\
+		<p>Heb12 Mobile is a free open-sourced app designed to make reading the Bible easy and hassle-free. If you would like to contribute or give feedback, please visit the&nbsp;<a href="https://github.com/heb12/heb12-mobile" target="_blank" rel="noopener">Github repository</a>.</p> \
+		<p><strong>Pro Tip: Tap on a bible verse for a list of extra actions.</strong></p>';
 	} else if (text == "settings") {
-		popup.innerHTML = `
-		<h2>Settings</h2>
-		<span class='textBesideSelect'>Theme:</span>
-		<select id='themeSelect' onchange="setTheme(this.value)">
-			<option>Default</option>
-			<option>Dark</option>
-			<option>Dark Blue</option>
-		</select>
-		<br>
-		<p id="fontPreview">The big brown fox jumps over the lazy dog.</p>
-		<span class='textBesideSelect'>Theme:</span>
-		<select id='fontSelect' onchange="setFont(this.value)">
-			<option>Times New Roman</option>
-			<option>Arial</option>
-			<option>Comfortaa</option>
-			<option>Helvetica</option>
-			<option>Courier New</option>
-		</select>
-		<br>
-		<div>
-			<div class='icon' style='float: left' onclick='setFontSize("minus")'>
-				<img src="images/left.svg" width="45">
-			</div>
-			
-			<div class='icon' style='float:left'>
-				<div style='width: 45px; text-align: center; padding-top: 10px; font-size: 20px;' id='fontSizeSelect'>
-					12
-				</div>
-			</div>
-
-			<div class='icon' style='float: left' onclick='setFontSize("plus")'>
-				<img src="images/right.svg" width="45">
-			</div>
-		</div>
-		<br>
-		<br>
-
-
-		<p>This will reset all your saved data. When reset the app, it will close so you can re-launch it.</p>
-		<br>
-		<div class='button bg' onclick='updateConfigFile("def"); interface.exec("other", "close")'>
-			Reset
-		</div>
-		`;
+		popup.innerHTML = "\
+		<h2>Settings</h2>\
+		<span class='textBesideSelect'>Theme:</span>\
+		<select id='themeSelect' onchange=\"setTheme(this.value)\">\
+			<option>Default</option>\
+			<option>Dark</option>\
+			<option>Dark Blue</option>\
+		</select>\
+		<br>\
+		<p id=\"fontPreview\">The big brown fox jumps over the lazy dog.</p>\
+		<span class='textBesideSelect'>Theme:</span>\
+		<select id='fontSelect' onchange=\"setFont(this.value)\">\
+			<option>Times New Roman</option>\
+			<option>Arial</option>\
+			<option>Comfortaa</option>\
+			<option>Helvetica</option>\
+			<option>Courier New</option>\
+		</select>\
+		<br>\
+		<div>\
+			<div class='icon' style='float: left' onclick='setFontSize(\"minus\")'>\
+				<img src=\"images/left.svg\" width=\"45\">\
+			</div>\
+			<div class='icon' style='float:left'>\
+				<div style='width: 45px; text-align: center; padding-top: 10px; font-size: 20px;' id='fontSizeSelect'>\
+					12\
+				</div>\
+			</div>\
+			<div class='icon' style='float: left' onclick='setFontSize(\"plus\")'>\
+				<img src=\"images/right.svg\" width=\"45\">\
+			</div>\
+		</div>\
+		<br>\
+		<br>\
+		<p>This will reset all your saved data. When reset the app, it will close so you can re-launch it.</p>\
+		<br>\
+		<div class='button bg' onclick='updateConfigFile(\"def\"); interface.exec(\"other\", \"close\")'>\
+			Reset\
+		</div>\
+		";
 
 		document.getElementById('page').style.fontSize = session.currentFontSize + "px";
 		document.getElementById('page').style.lineHeight = (session.currentFontSize + 7) + "px";
@@ -323,24 +318,24 @@ function notify(text) {
 		document.getElementById('fontSelect').value = session.currentFont;
 		document.getElementById('fontSizeSelect').innerHTML = session.currentFontSize;
 	} else if (text == "info") {
-		popup.innerHTML = `
-		<img style="display:inline; float:left; margin-right:10px;" src="images/logo.png" width="150">
-		<div style="display:inline;">
-		   <h2>Heb12 Mobile v1.0</h2>
-		   <p>
-		   Heb12 Mobile is a free open-sourced app designed to make reading the bible easy and hassle-free. Feel free to contribute to the 
-		   <a target="_blank" href="https://github.com/heb12/heb12-mobile">Github repository</a>.
-		   </p>
-		</div>
-		<br>
-		<p><a style='color: blue; text-decoration: underline;' onclick="document.getElementById('page').innerHTML = load('Hebrews', 4); notify('verse-12')">Hebrews 4:12</a> - For the word of God is living, and powerful, and sharper than any two-edged sword, piercing even to the dividing asunder of soul and spirit, and of the joints and marrow, and is a discerner of the thoughts and intents of the heart.</p>
-		<br>
-		<h2>Credits</h2>
-		<p>Lead Programmer - Pufflegamerz aka Petabyte Studios</p>
-		<p>Openbibles - MasterOfTheTiger</p>
-		<p>Material icons - Material.io</p>
-		<p>Formatted KJV API - Bible Labs</p>
-		<p><a href="https://github.com/thiagobodruk" target="_blank">@thiagobodruk</a> - Offline Bible JSON files</p>`;
+		popup.innerHTML = "\
+		<img style=\"display:inline; float:left; margin-right:10px;\" src=\"images/logo.png\" width=\"150\">\
+		<div style=\"display:inline;\">\
+		   <h2>Heb12 Mobile v1.0</h2>\
+		   <p>\
+		   Heb12 Mobile is a free open-sourced app designed to make reading the bible easy and hassle-free. Feel free to contribute to the \
+		   <a target=\"_blank\" href=\"https://github.com/heb12/heb12-mobile\">Github repository</a>.\
+		   </p>\
+		</div>\
+		<br>\
+		<p><a style='color: blue; text-decoration: underline;' onclick=\"document.getElementById('page').innerHTML = load('Hebrews', 4); notify('verse-12')\">Hebrews 4:12</a> - For the word of God is living, and powerful, and sharper than any two-edged sword, piercing even to the dividing asunder of soul and spirit, and of the joints and marrow, and is a discerner of the thoughts and intents of the heart.</p>\
+		<br>\
+		<h2>Credits</h2>\
+		<p>Lead Programmer - Pufflegamerz aka Petabyte Studios</p>\
+		<p>Openbibles - MasterOfTheTiger</p>\
+		<p>Material icons - Material.io</p>\
+		<p>Formatted KJV API - Bible Labs</p>\
+		<p><a href=\"https://github.com/thiagobodruk\" target=\"_blank\">@thiagobodruk</a> - Offline Bible JSON files</p>";
 	} else if (text == 'hehe') {
 		session.titleClicks++
 		if (session.titleClicks >= 10) {
@@ -687,7 +682,7 @@ function modifyVerse(verseNum, verseText) {
 		color = item;
 	}
 
-	return `<span class='verse' onclick='versePopup(` + verseNum + `)'><b id='verse'>` + (verseNum + 1) + `</b> <span style='background: ` + color + `;' class='verseText'>` + verseText + `</span></span>` + "<br>".repeat(session.breaksAfterVerse);
+	return "<span class='verse' onclick='versePopup(" + verseNum + ")'><b id='verse'>" + (verseNum + 1) + "</b> <span style='background: " + color + ";' class='verseText'>" + verseText + "</span></span>" + "<br>".repeat(session.breaksAfterVerse);
 }
 
 // Show highlighting menu
@@ -732,9 +727,9 @@ function versePopup(verse) {
 			session.currentVerse = entire;
 
 			mainContent.getElementsByTagName("SPAN")[0].innerHTML = theVerse;
-			document.getElementById("verseMenu").children[1].children[0].children[0].setAttribute("onclick", `search("` + theVerseText + `")`);
-			document.getElementById("verseMenu").children[1].children[1].children[0].setAttribute("onclick", `interface.exec('copy',"` + theVerseText + ` - ` + theVerse + `")`);
-			document.getElementById("verseMenu").children[1].children[2].children[0].setAttribute("onclick", `interface.exec('share',"` + theVerseText + ` - ` + theVerse + `")`);
+			document.getElementById("verseMenu").children[1].children[0].children[0].setAttribute("onclick", 'search("' + theVerseText + '")');
+			document.getElementById("verseMenu").children[1].children[1].children[0].setAttribute("onclick", "interface.exec('copy'," + theVerseText + " - " + theVerse + ")");
+			document.getElementById("verseMenu").children[1].children[2].children[0].setAttribute("onclick", "interface.exec('share'," + theVerseText + " - " + theVerse + ")");
 
 			session.doneLoadingJSON = false;
 			clearInterval(wait);
